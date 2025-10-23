7c8d29589c42e7db17f8e97d084f26eeb1ac1dec
Esteban Aguero Perez <esteban.aguero@northern.tech>
2025-10-23 16:24:25 -0600

    Fix cleanup script
    Signed-off-by: Esteban Aguero Perez <esteban.aguero@northern.tech>

--- cleanup.sh
+++ cleanup.sh
@@ -5,4 +5,7 @@
 docker stop mender-gateway
 docker rm mender-gateway
 
-find . -maxdepth 1 -type f -not \( -name "*.sh" -o -name "*.md" \) -delete
+docker stop mender-client-certificates
+docker rm mender-client-certificates
+
+find . -maxdepth 1 -type f -not \( -name "*.sh" -o -name "*.md" -o -name "*.gitignore" \) -delete
