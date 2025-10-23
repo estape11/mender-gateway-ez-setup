319df7aeafa532a9894e054773b167a154ff3173
Esteban Aguero Perez <esteban.aguero@northern.tech>
2025-10-23 16:34:18 -0600

    Se agrega .env al gitignore
    Signed-off-by: Esteban Aguero Perez <esteban.aguero@northern.tech>

--- cleanup.sh
+++ cleanup.sh
@@ -8,4 +8,4 @@ docker rm mender-gateway
 docker stop mender-client-certificates
 docker rm mender-client-certificates
 
-find . -maxdepth 1 -type f -not \( -name "*.sh" -o -name "*.md" -o -name "*.gitignore" \) -delete
+find . -maxdepth 1 -type f -not \( -name "*.sh" -o -name "*.md" -o -name "*.gitignore" -o -name "*.env" \) -delete
